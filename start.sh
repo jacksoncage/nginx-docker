6a8e64cc5885a2a453a27d8a7666d1cc9b2e8fd7
Love Nyberg <love.nyberg@viaplay.com>
2014-02-07 13:39:14 +0100

    Added start.sh file after remove.

--- start.sh
+++ start.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 
-# Setup backend/cache port
-if [[ -z "$ENV_PORT" ]] ; then
-    sed -ie 's/server\ 172\.17\.42\.1\:80/server\ 172\.17\.42\.1\:$ENV_PORT/g' /etc/nginx/sites-available/tennistunering.se
-fi
-
 # Start nginx
 /usr/sbin/nginx -D FOREGROUND
